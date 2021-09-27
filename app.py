6e691015d1fada86e248c287d5f0daa5f1352aa1
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-27 20:01:07 +0300

    getting all recipes endpoint done

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
-from flask import Flask
+
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+import os
 app = Flask(__name__)
 app.secret_key = "mkuu"
 api = Api(app)
 
 
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql+psycopg2://developer:developerwilson@localhost/recipe_app"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
-
 from models import Recipe
+
+
+
+
+
+
+class RecipeList(Resource):
+    # get all recipes
+    def get(self):
+        return {"recipes": [recipe.json() for recipe in Recipe.query.all()]}
+
+
+
+
+# set the route and accepted methods
+api.add_resource(RecipeList, "/recipes")
 
 if __name__ == "__main__":
     app.run(debug=True)
