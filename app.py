26876ff709e0a134135cf7735d72ba6a3299c9fc
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-28 22:52:55 +0300

    getting all recipes done

--- app.py
+++ app.py
@@ -1,36 +1,58 @@
-
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+
+
 app = Flask(__name__)
 app.secret_key = "mkuu"
 api = Api(app)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://developer:developerwilson@localhost/recipe_app'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
+
+
 from models import Recipe
 
+class SingleRecipe(Resource):
+    def get(self, node_id):
+        recipe = Recipe.query.filter_by(node_id=node_id).first()
+        if recipe:
+            return recipe.to_json()
+        else:
+            return {"message": "Recipe not found"}, 404
 
-
-
+    # post method to add recipe to database
+    def post(self):
+        data = request.get_json()
+        recipe = Recipe(
+            # node_id = uuid.uuid4().hex[:6],
+            name=data["name"],
+            description=data["description"],
+            instructions=data["instructions"],
+            cook_time=data["cook_time"],
+            imageUrl=data["imageUrl"],
+            originalUrl=data["originalUrl"]
+        )
+        print(recipe)
+        db.session.add(recipe)
+        db.session.commit()
+        return recipe.to_json()
 
 
 class RecipeList(Resource):
     # get all recipes
     def get(self):
-        return {"recipes": [recipe.json() for recipe in Recipe.query.all()]}
-
-
-
+        return {"recipes": [recipe.to_json() for recipe in Recipe.get_all()]}
 
 # set the route and accepted methods
 api.add_resource(RecipeList, "/recipes")
-
+api.add_resource(SingleRecipe, "/recipe")
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,28 +1,25 @@
-from datetime import datetime
 from app import db as db
-
+import uuid
 
 # Model for the table 'Recipe'
+
+
 class Recipe(db.Model):
     __tablename__ = 'recipe'
     id = db.Column(db.Integer, primary_key=True)
+    node_id = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500), nullable=False)
-    ingredients = db.Column(db.String(500), nullable=False)
     instructions = db.Column(db.String(500), nullable=False)
-    cook_time = db.Column(db.Integer, nullable=False)
+    cook_time = db.Column(db.String(255), nullable=False)
     imageUrl = db.Column(db.String(100), nullable=False)
     originalUrl = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow)
 
-    def __init__(self, name, description, ingredients, instructions,
+    def __init__(self, name, description, instructions,
                  cook_time, imageUrl, originalUrl):
+        self.node_id = uuid.uuid4().hex[:20]
         self.name = name
         self.description = description
-        self.ingredients = ingredients
         self.instructions = instructions
         self.cook_time = cook_time
         self.imageUrl = imageUrl
@@ -32,31 +29,25 @@ class Recipe(db.Model):
     def to_json(self):
         return {
             'id': self.id,
+            "node_id": self.node_id,
             'name': self.name,
             'description': self.description,
-            'ingredients': self.ingredients,
             'instructions': self.instructions,
             'cook_time': self.cook_time,
             'imageUrl': self.imageUrl,
-            'originalUrl': self.originalUrl,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at
+            'originalUrl': self.originalUrl
         }
-
+    # Base.query = session.query_property()
     # Method to get a single recipe by id
+
     @staticmethod
-    def get_by_id(id):
-        return Recipe.query.filter_by(id=id).first()
+    def get_by_id(node_id):
+        return Recipe.query.filter_by(node_id=node_id).first()
 
     # Method to get all recipes
     @staticmethod
     def get_all():
         return Recipe.query.all()
-
-    # Method to add a new recipe
-    def add(self):
-        db.session.add(self)
-        db.session.commit()
 
     # Method to update a recipe
     def update(self):
@@ -69,3 +60,51 @@ class Recipe(db.Model):
 
     def __repr__(self):
         return '<Recipe %r>' % self.name
+
+
+# Model for the table 'Ingredient'
+class Ingredient(db.Model):
+    __tablename__ = 'ingredient'
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.String(100), nullable=False)
+    unit = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name, quantity, unit, recipe_id):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+        self.recipe_id = recipe_id
+
+    # Method to return the ingredient as a json
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'quantity': self.quantity,
+            'unit': self.unit,
+            'recipe_id': self.recipe_id,
+        }
+
+    # Method to get a single ingredient by id
+    @staticmethod
+    def get_by_id(id):
+        return Ingredient.query.filter_by(id=id).first()
+
+    # Method to get all ingredients
+    @staticmethod
+    def get_all():
+        return Ingredient.query.all()
+
+    # Method to update an ingredient
+    def update(self):
+        db.session.commit()
+
+    # Method to delete an ingredient
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Ingredient %r>' % self.name
